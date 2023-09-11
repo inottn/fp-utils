ff2cd1a707e909fa018716c6119bead20c5c7a27
inottn <inottn@outlook.com>
2023-09-11 11:28:08 +0800

    types: improve ts def

--- src/utils.ts
+++ src/utils.ts
@@ -7,10 +7,10 @@ export const isObject = (val: unknown): val is Record<any, any> =>
 export const isPromise = <T = any>(val: unknown): val is Promise<T> =>
   isObject(val) && isFunction(val.then) && isFunction(val.catch);
 
-export const isNonEmptyArray = (val: unknown) =>
+export const isNonEmptyArray = (val: unknown): val is unknown[] =>
   Array.isArray(val) && val.length > 0;
 
-export const isEmptyArray = (val: unknown) =>
+export const isEmptyArray = (val: unknown): val is unknown[] =>
   Array.isArray(val) && val.length === 0;
 
 export const isUndefined = (val: unknown): val is undefined =>
